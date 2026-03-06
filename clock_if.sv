c6229964b5b687d08c4fb3a32d23842890ca79d1
kavitag-0325 <kavitag.ec23@bmsce.ac.in>
2026-03-06 09:44:06 +0530

    Update clock_if.sv

--- clock_if.sv
+++ clock_if.sv
@@ -1,4 +1,11 @@
-`timescale 1ns/1ps
+// -----------------------------------------------------------------------------
+// File        : clock_if.sv
+// Author      : Kavita G Panjagall(1BM23EC118)
+// Created     : 2026-01-28
+// Module      : clock_if
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+// ----------------------------------------------------------------------------- `timescale 1ns/1ps
 
 interface clock_if (input logic clk);
 
